78f8f21f311a2b5584c4ab227b2c452d9cc8cc9c
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-11-05 21:48:09 +0900

    ハンバーガーとメニュー追加

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$('#hamburger-btn').on('click',function(){
+	$(this).toggleClass('on');
+	});
+
+	$('#hamburger-btn').on('click',function(){
+	$('.menu-target').toggleClass('hide');
+	});
+
+	});
+
